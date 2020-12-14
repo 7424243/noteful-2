0c70ba5166a4e783f88725bfabd4328d5f28210f
Sam Sheets <sam.sheets94@gmail.com>
2020-12-14 08:47:38 -0700

    initial commit and header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Link to='/' className='header-link'><h1>Noteful</h1></Link>
       </header>
     </div>
   );
