7135cd143cf737a77543a302929030cef481ad05
Sam Sheets <sam.sheets94@gmail.com>
2020-12-14 09:06:49 -0700

    NoteListNav component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React, {Component} from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import DUMMYSTORE from './dummy-store';
+import NotesListNav from './Components/NoteListNav';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <Link to='/' className='header-link'><h1>Noteful</h1></Link>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    notes: [],
+    folders: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      notes: DUMMYSTORE.notes,
+      folders: DUMMYSTORE.folders
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <Link to='/' className='header-link'><h1>Noteful</h1></Link>
+        </header>
+        <nav>
+        <Route 
+            exact
+            path='/' 
+            render={() => <NotesListNav 
+              folders={this.state.folders}
+              notes={this.state.notes}/>}
+          />
+        </nav>
+        <main></main>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/Components/NoteListNav.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom';
+
+class NoteListNav extends Component {
+    render() {
+
+        const list = this.props.folders.map((folder, i) => 
+
+            <li 
+                key={i} 
+                id={folder.id} 
+                className='folder'>
+                <NavLink 
+                    to={`../noteslist/${folder.id}`}
+                    className='folder-name'
+                    foldername={folder.name}>{folder.name}</NavLink>
+            </li>
+        );
+
+        return (
+            <div className='folders-container'>
+                <ul className='folder-list'>
+                    {list}
+                </ul>
+                <NavLink 
+                    to='/addfolder' 
+                    className='add-folder-link'
+                    >Add Folder</NavLink>
+            </div>
+        )
+    }
+}
+
+export default NoteListNav;
