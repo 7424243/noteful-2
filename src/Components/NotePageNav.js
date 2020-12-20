bbdc71b2fc1aed538bb6fde15b236fd9e966eeda
Sam Sheets <sam.sheets94@gmail.com>
2020-12-20 15:39:50 -0700

    NotePageNav PropTypes

--- src/Components/NotePageNav.js
+++ src/Components/NotePageNav.js
@@ -1,19 +1,15 @@
 import React, { Component } from "react";
 import {withRouter} from 'react-router-dom';
 import NotefulContext from "../NotefulContext";
+import PropTypes from 'prop-types';
 import '../Styling/NotePageNav.css';
 
 class NotePageNav extends Component {
-    static defaultProps = {
-        match: {
-          params: {}
-        }
-      }
 
     static contextType = NotefulContext;
 
     render() {
-        const {noteId} = this.props.match.params;
+        const noteId = this.props.match.params.noteId;
         const {notes=[]} = this.context;
         const {folders=[]} = this.context;
         const getNote = (notes, noteId) =>
@@ -35,4 +31,15 @@ class NotePageNav extends Component {
 }
 
 export default withRouter(NotePageNav);
+
+NotePageNav.propTypes = {
+    match: PropTypes.shape({
+        params: PropTypes.shape({
+            noteId: PropTypes.string
+        })
+    }).isRequired,
+    history: PropTypes.shape({
+        goBack: PropTypes.func
+    }).isRequired
+}
 
