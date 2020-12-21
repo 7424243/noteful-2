00df43612c3020c4828c0ce7f1af52b8e004152f
Sam Sheets <sam.sheets94@gmail.com>
2020-12-21 14:10:43 -0700

    added comments to App and ErrorBoundary components

--- src/App.js
+++ src/App.js
@@ -10,16 +10,19 @@ import AddNote from './Components/AddNote';
 import ErrorBoundary from './Components/ErrorBoundary';
 
 class App extends Component {
+  //set state
   state = {
     notes: [],
     folders: [],
   }
-
+  //fetch api data inside componentDidMount method
   componentDidMount() {
+    //Promise.all() runs several asynchronous operations in parallel. Accepts an array of promises/fetches and ensures all are resolved before invoking the then() method.
     Promise.all([
       fetch(`http://localhost:9090/notes`),
       fetch(`http://localhost:9090/folders`)
     ])
+        //the parameter of then() is an array of results of the Promises.
         .then(([notesResponse, foldersResponse]) => {
           if (!notesResponse.ok) 
             return notesResponse.json().then(error => Promise.reject(error));
@@ -28,33 +31,37 @@ class App extends Component {
           return (Promise.all([notesResponse.json(), foldersResponse.json()])) ;
         })
         .then(([notes, folders]) => {
-          this.setState({notes, folders});
+          this.setState({notes, folders}); //update State with response data
           
         })
         .catch(error => {
-          console.error({error});
+          console.error({error}); //this catch logs the error object in the console
         });
   }
-
+  //this function uses class property syntax in order to bind this function to state using 'this'
+  //this function updates state to include notes that do not have a particular noteId
   handleDeleteNote = noteId => {
     this.setState({
       notes: this.state.notes.filter(note => note.id !== noteId)
     });
   }
-
+  //this function uses class property syntax in order to bind this function to state using 'this'
+  //this function updates state to include a newly created folder
   addFolder = folder => {
     this.setState({
-      folders: [ ...this.state.folders, folder],
+      folders: [ ...this.state.folders, folder],//[...]='spread syntax' (can be used when alements from an object or array need to be included in a lsit of some kind)
     });
   }
-
+  //this function uses class property syntax in order to bind this function to state using 'this'
+  //this function updates state to include a newly created note
   addNote = note => {
     this.setState({
-      notes: [ ...this.state.notes, note],
+      notes: [ ...this.state.notes, note],//[...]='spread syntax' (can be used when alements from an object or array need to be included in a lsit of some kind)
     });
   }
-
+  //render method is required for class components
   render() {
+    //set the context values to the data that is currently in state and include the event handler functions
     const contextValue = {
       notes: this.state.notes,
       folders: this.state.folders,
@@ -65,20 +72,20 @@ class App extends Component {
     return (
       <div className="App">
         <header>
-          <Link to='/' className='header-link'><h1>Noteful</h1></Link>
+          <Link to='/' className='header-link'><h1>Noteful</h1></Link>{/*A Link instead of a header because this component is wrapper in Router*/}
         </header>
-        <NotefulContext.Provider value={contextValue} >
+        <NotefulContext.Provider value={contextValue} >{/*Wrap code in context provider; any component that wants to read the context needs to be a descendent of the provider. Include a value prop in order to actually pass/hold the context data*/}
           <div className='nav-main'>
             <ErrorBoundary>
               <nav>
                 <Route 
-                  exact
-                  path='/' 
-                  component={NoteListNav}
+                  exact //this keyword ensures that you only display one route at a time -- we only want this exact component to show for exact components
+                  path='/'  //home path
+                  component={NoteListNav} //this is the component that should render in the home path
                 />
                 <Route
-                  path='/noteslist/:folderId'
-                  component={NoteListNav}
+                  path='/noteslist/:folderId' //this is a dynamic route path
+                  component={NoteListNav} 
                 />  
                 <Route 
                   path='/notepage/:noteId'
--- src/Components/ErrorBoundary.js
+++ src/Components/ErrorBoundary.js
@@ -1,18 +1,22 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+//an Error Boundary is a component that can catch a JS error anywhere in the child component tree and log it and display an error message instead of crashing the whole app.
+
 class ErrorBoundary extends Component {
+    //set default state so that hasError is false.
     constructor(props) {
         super(props);
         this.state = {
             hasError: false
         };
     }
+    //update state so that the next render will show the fallback UI
     static getDerivedStateFromError(error) {
         return {hasError: true, error};
     }
 
-
+    //render the UI based on hasError
     render() {
 
         if (this.state.hasError) {
@@ -27,6 +31,7 @@ class ErrorBoundary extends Component {
 
 export default ErrorBoundary;
 
+//set PropTypes for validation
 ErrorBoundary.propTypes = {
     children: PropTypes.element.isRequired
 }
