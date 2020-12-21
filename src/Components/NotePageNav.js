78bbd790760985f85d360ab4ac55fe6198e56960
Sam Sheets <sam.sheets94@gmail.com>
2020-12-21 14:18:05 -0700

    included comments in NotePageNav

--- src/Components/NotePageNav.js
+++ src/Components/NotePageNav.js
@@ -5,22 +5,22 @@ import PropTypes from 'prop-types';
 import '../Styling/NotePageNav.css';
 
 class NotePageNav extends Component {
-
+    // set contextType to the app's context in order to access it and use it.
     static contextType = NotefulContext;
 
     render() {
-        const noteId = this.props.match.params.noteId;
-        const {notes=[]} = this.context;
-        const {folders=[]} = this.context;
+        const noteId = this.props.match.params.noteId; //get noteId from Route's props
+        const notes = this.context.notes; //get notes from context
+        const folders = this.context.folders; //get folders from context
         const getNote = (notes, noteId) =>
-            notes.find(note => note.id === noteId);
-        const noteForPage = getNote(notes, noteId);
+            notes.find(note => note.id === noteId); //get a specific note by passing in all of the notes and the noteId and finding the note that has the noteId
+        const noteForPage = getNote(notes, noteId); //pass in notes and noteId
          const currentFolder = folders.find(folder => 
-             folder.id === noteForPage.folderId);
+             folder.id === noteForPage.folderId); //get the current folder by finding the folder that has the note's folderId
         return (
             <nav className='note-nav'>
                 <button 
-                    onClick={() => this.props.history.goBack()}
+                    onClick={() => this.props.history.goBack()} //use Route's history to go back
                     className='note-page-back-link'>
                         back
                 </button>
@@ -32,14 +32,15 @@ class NotePageNav extends Component {
 
 export default withRouter(NotePageNav);
 
+//define PropTypes for validation
 NotePageNav.propTypes = {
-    match: PropTypes.shape({
+    match: PropTypes.shape({ //.shape is used when you have a nested key/value pair
         params: PropTypes.shape({
             noteId: PropTypes.string
         })
     }).isRequired,
     history: PropTypes.shape({
         goBack: PropTypes.func
-    }).isRequired
+    }).isRequired //.isRequired, always need it if the app will crash without it.
 }
 
