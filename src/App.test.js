aea11fd7f827e55c3f1f639f3516d147c2d3807a
Sam Sheets <sam.sheets94@gmail.com>
2020-12-21 13:21:53 -0700

    added comments in App.test and index.js

--- src/App.test.js
+++ src/App.test.js
@@ -5,12 +5,15 @@ import App from './App';
 
 
 it('renders without crashing', () => {
+  //create a div in which to render the component
   const div = document.createElement('div')
+  //render the component wrapped in BrowserRouter (as it includes Route)
   ReactDOM.render(
     <BrowserRouter>
       <App/>
     </BrowserRouter>,
     div
   )
+  //clean up the code by unmounting the App component from the DOM
   ReactDOM.unmountComponentAtNode(div)
 })
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 
-
+//render the App Component. 
+//Wrap with BrowserRouter (access to forward and back buttons using browser, allows us to render components depending on the URL's path)
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root')//'root' id is found in index.html inside of public folder.
 );
